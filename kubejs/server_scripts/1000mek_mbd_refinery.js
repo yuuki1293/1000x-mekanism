5906f6b8d948c7b938b88945e04519e786200e71
HardSmoothy <zouliangt@gmail.com>
2024-12-15 22:57:54 +0900

    HS#9d What's happened????
    なんでint圏外だと動くんですか？？？？？

--- kubejs/server_scripts/1000mek_mbd_refinery.js
+++ kubejs/server_scripts/1000mek_mbd_refinery.js
@@ -1,33 +1,21 @@
 onEvent('recipes', event => {
 
+    
     // ore refinerys
-    let hyper_refinery = (req)=>{
-        event.recipes.multiblocked.multiblock("hyper_refinery")
-        .inputItem(req.itemInput)
-        .inputGas({type:req.gasInput.type, amount:req.gasInput.amount})
-        .inputFluid(Fluid.of(req.fluidInput.type, req.fluidInput.amount))
-        .outputItem(`${req.itemOutput}`)
+    event.recipes.multiblocked.multiblock("large_ore_refinery")
+        .inputItem("minecraft:iron_ore")
+        .inputGas({type:"mekanism:sulfuric_acid",amount:20000000000})
+        .inputFluid(Fluid.of("minecraft:water",200))
+        .outputItem("8x mek1000:iron_shard_20")
         .setPerTick(true)
-        .inputFE(req.consume)
-        //.duration(1)
-    }
-    hyper_refinery({
-        itemInput: "minecraft:iron_ore",
-        gasInput: {type:"mekanism:sulfuric_acid",amount:2},
-        fluidInput: {type:"minecraft:water", amount:200},
-        itemOuput: "8x mek1000:iron_shard_20",
-        consume: 1000, duration: 20,
-    }) //test
-    hyper_refinery({
-        itemInput: "minecraft:iron_ore",
-        gasInput: {type:"mek1000:active_aqua_regia",amount:2000},
-        fluidInput: {type:"minecraft:water", amount:200},
-        itemOuput: "12x mek1000:compressed_purified_${type}_rawore",
-        consume: 20000, duration: 50,
-    }) //test
-
-
-
-
-
+        .inputFE(1000)
+        //.duration(20)
+    event.recipes.multiblocked.multiblock("large_ore_refinery")
+        .inputItem("minecraft:iron_ore")
+        .inputGas({type:"mek1000:active_aqua_regia",amount:20000000000})
+        .inputFluid(Fluid.of("minecraft:water",200))
+        .outputItem("12x mek1000:compressed_purified_iron_rawore")
+        .setPerTick(true)
+        .inputFE(20000)
+        //.duration(50)
 })
